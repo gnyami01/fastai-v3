c4580ab8a5cde3da0eaffaa9233706c2a2dbe9c3
gnyami01 <gnyami01@mail.bbk.ac.uk>
2019-05-23 22:01:03 +0000

    Update server.py
    Configured model URL and class names.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1ZIetBgMtHmnGd5Uyk4kRBH7P2Mtlt9NF'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['breitling', 'patek_phillipe', 'rolex']
 path = Path(__file__).parent
 
 app = Starlette()
