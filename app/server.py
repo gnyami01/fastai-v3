cb6090626ed19bceda67048fc64dbaa80fa01b89
gnyami01 <gnyami01@mail.bbk.ac.uk>
2019-06-11 13:43:28 +0100

    Update server.py
    Add bear model link

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1ZIetBgMtHmnGd5Uyk4kRBH7P2Mtlt9NF'
+#export_file_url = 'https://drive.google.com/uc?export=download&id=1ZIetBgMtHmnGd5Uyk4kRBH7P2Mtlt9NF'
+export_file_url = 'https://drive.google.com/uc?export=download&id=11FL01fft89u4Mm1NEIijXWMj29tuBr-Z'
 export_file_name = 'export.pkl'
 
-classes = ['breitling', 'patek_phillipe', 'rolex']
+classes = ['black', 'teddys', 'grizzly']
 path = Path(__file__).parent
 
 app = Starlette()
